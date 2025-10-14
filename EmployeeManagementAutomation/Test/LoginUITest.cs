88365862a0f52dcc33c88e8f6d97cfa1e607c7c4
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-14 11:22:57 +0530

    Add project files.

---
+++ EmployeeManagementAutomation/Test/LoginUITest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementAutomation.Test
+{
+    public class LoginUITest
+    {
+
+        [Test]
+        public void TitleTest()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = @"D:\Balaji\Components\chrome-win64\chrome-win64\chrome.exe";
+
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
+
+            Assert.That(driver.Title, Is.EqualTo("OrangeHRM"));
+        }
+
+        //configure local repo and commit
+    }
+}
