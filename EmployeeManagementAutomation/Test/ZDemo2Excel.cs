9955fbbf49e3e5858ce551dcdb11e1f8125836cd
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-15 12:55:44 +0530

    Complete - Read all cell value and print it

---
+++ EmployeeManagementAutomation/Test/ZDemo2Excel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+using EmployeeManagementAutomation.Utilities;
+
+namespace EmployeeManagementAutomation.Test
+{
+    public class ZDemo2Excel
+    {
+        [Test]
+        public void ReadExcelTest()
+        {
+            XLWorkbook book = new XLWorkbook(@"D:\Balaji\Company\Siemens Oct 2025\AutomationFramework\EmployeeManagementAutomation\TestData\orange-test-data.xlsx");
+
+            var sheet = book.Worksheet("InvalidLoginTest");
+
+            var range = sheet.RangeUsed();
+
+            string cellValue = range.Cell(1, 2).GetString();
+            Console.WriteLine(cellValue);
+
+            //read all cell value and print it
+
+            book.Dispose();
+
+        }
+    }
+}
