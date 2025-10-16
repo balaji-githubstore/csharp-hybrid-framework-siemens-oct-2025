13378b154a52dc5c8303adf99914756d3c47d149
balaji-githubstore <balajidinakaran1@gmail.com>
2025-10-16 10:42:40 +0530

    Updated Code for Excel and TestCaseSource

--- EmployeeManagementAutomation/Test/ZDemo2Excel.cs
+++ EmployeeManagementAutomation/Test/ZDemo2Excel.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Wordprocessing;
+using EmployeeManagementAutomation.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClosedXML.Excel;
-using EmployeeManagementAutomation.Utilities;
 
 namespace EmployeeManagementAutomation.Test
 {
@@ -13,32 +14,32 @@ namespace EmployeeManagementAutomation.Test
     /// </summary>
     public class ZDemo2Excel
     {
-        //[Test]
+        [Test]
         public void ReadExcelTest()
         {
             XLWorkbook book = new XLWorkbook(@"TestData/orange-test-data.xlsx");
 
-            var sheet = book.Worksheet("InvalidLoginTest");
+            var sheet = book.Worksheet("AddValidEmployeeTest");
 
             var range = sheet.RangeUsed();
 
             int rowCount = range.RowCount();
             int colCount=range.ColumnCount();
 
-            object[] allData = new object[rowCount-1];
+            object[] allData=new object[2];
 
             //read all cell value and print it
             for (int r=2;r<=rowCount;r++)
             {
-                string[] data = new string[colCount];
+                string[] arr = new string[colCount];
                 for(int c=1;c<=colCount;c++)
                 {
                     string cellValue = range.Cell(r, c).GetString();
                     Console.WriteLine(cellValue);
-                    data[c-1] = cellValue;
+                    arr[c-1] = cellValue;
                 }
 
-                allData[r-2]=data;
+                allData[r-2] = arr;
             }
 
 
